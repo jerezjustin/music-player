c48f2c25ad329fa18ccc3ba465f6bcbb98c58d40
Justin Jerez <justinjerez@hotmail.com>
2023-08-08 15:10:50 -0400

    Configure VitePWA

--- src/main.ts
+++ src/main.ts
@@ -8,9 +8,12 @@ import VeeValidate from './includes/validation'
 import { auth } from './includes/firebase'
 import Icon from './directives/icon'
 import i18n from './includes/i18n'
+import { registerSW } from 'virtual:pwa-register'
 
 import App from './App.vue'
 import router from './router'
+
+registerSW({ inmedita: true })
 
 let app: AppVue
 
--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,33 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    plugins: [
+        vue(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            devOptions: {
+                enabled: true
+            },
+            manifest: {
+                name: 'Music App',
+                theme_color: '#ff5e3a',
+                icons: [
+                    {
+                        src: 'assets/img/pwa-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png'
+                    }
+                ]
+            }
+        })
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
     }
-  }
 })
