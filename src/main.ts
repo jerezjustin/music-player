2fe407338753edd812758d587da3cb615aac0b0f
Justin Jerez <justinjerez@hotmail.com>
2023-08-21 11:50:23 -0400

    Performance optimizations

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import './assets/base.css'
 import './assets/main.css'
+import 'nprogress/nprogress.css'
 
 import { createApp, type App as AppVue } from 'vue'
 import { createPinia } from 'pinia'
@@ -9,11 +10,15 @@ import { auth } from './includes/firebase'
 import Icon from './directives/icon'
 import i18n from './includes/i18n'
 import { registerSW } from 'virtual:pwa-register'
+import GlobalComponents from './includes/_globals'
+import progressBar from './includes/progress-bar'
 
 import App from './App.vue'
 import router from './router'
 
 registerSW({ inmedita: true })
+
+progressBar(router)
 
 let app: AppVue
 
@@ -25,6 +30,7 @@ auth.onAuthStateChanged(() => {
         app.use(router)
         app.use(VeeValidate)
         app.use(i18n)
+        app.use(GlobalComponents)
 
         app.directive('icon', Icon)
 
