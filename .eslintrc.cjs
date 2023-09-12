494939bf474f98b055dccc6cacc39918f4df02f8
Justin Jerez <justinjerez@hotmail.com>
2023-09-12 15:53:44 -0400

    Configure vitest

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,24 +2,31 @@
 require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
-  root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
-  ],
-  overrides: [
-    {
-      files: [
-        'cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}'
-      ],
-      'extends': [
-        'plugin:cypress/recommended'
-      ]
+    root: true,
+    extends: [
+        'plugin:vue/vue3-essential',
+        'eslint:recommended',
+        '@vue/eslint-config-typescript',
+        '@vue/eslint-config-prettier/skip-formatting'
+    ],
+    overrides: [
+        {
+            files: ['cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}'],
+            extends: ['plugin:cypress/recommended']
+        },
+        {
+            files: ['src/components/__tests__/**spec.js'],
+            globals: {
+                test: 'readonly',
+                describe: 'readonly',
+                expect: 'readonly',
+                vi: 'readonly',
+                beforeEach: 'readonly',
+                it: 'readonly'
+            }
+        }
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest'
     }
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest'
-  }
 }
